c615e4f7c4e90a9185611d7e7b80570bd82d19ac
Alison <alison_francisco03@hotmail.com>
2018-03-24 08:35:21 -0300

    coloquei o Favicon, Navbar,footer.

--- Projeto02Servlets/src/java/br/com/fatecpg/ads/poo/servlets/JurosSimples.java
+++ Projeto02Servlets/src/java/br/com/fatecpg/ads/poo/servlets/JurosSimples.java
@@ -36,18 +36,87 @@ public class JurosSimples extends HttpServlet {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
+            
+    /* Style Test */
+            out.println("<style>.p{font-family: 'Orbitron', sans-serif;}</style>");
+            out.println("<style>.p2{font-family: 'Advent Pro', sans-serif;}</style>");
+            out.println("<style>.fim {\n" +"    position:absolute;\n" +"    bottom:0;\n" +"    width:100%;\n" +"}</style>");
+    /* Fim Style Test */  
+    
             out.println("<head>");
             out.println("<title>Servlet JurosSimples</title>");
-            out.println("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">");            
+            out.println("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">");
+    /* ---- CSS ------ */
+            out.println("<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css\" integrity=\"sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm\" crossorigin=\"anonymous\">");
+    /* ----Fim CSS ------- */
+    
+    /* ------ FAVICON -----*/
+            out.println("<link rel=\"shortcut icon\" href=\"Imagens/favicon.png\" />");
+    /*---Fim do FAVICON --- */
+    
+    /* ---- Google Fonts ----*/
+            out.println("<link href=\"https://fonts.googleapis.com/css?family=Orbitron\" rel=\"stylesheet\">");
+            out.println("<link href=\"https://fonts.googleapis.com/css?family=Advent+Pro\" rel=\"stylesheet\"> ");
+    /* ------ Fim Fonts -----*/
+    
             out.println("</head>");
             out.println("<body>");
+    /*--- NAVBAR ---*/
+            out.println("<div class=\"pos-f-t\">\n" +
+                        "  <div class=\"collapse\" id=\"navbarToggleExternalContent\">\n" +
+                        "    <div class=\"bg-dark p-4\">\n" +
+                        "      <h4 class=\"text-white\">CONTinf</h4>\n" +
+                        "      <span class=\"text-muted\">Toggleable via the navbar brand.</span>\n" +
+                        "    </div>\n"/* Botoes da navbar*/ +"<ul class=\"nav nav-tabs navbar-dark bg-dark\">\n"
+                        +" <li class=\"nav-item\">\n"
+                        +"  <a class=\"nav-link active\" href=\"home.html\">HOME</a>\n"
+                        +"</li>\n"
+                        +"<li class=\"nav-item\">\n"
+                        +" <a class=\"nav-link text-white\" href=\"jurossimples.html\">Juros Simples</a>\n"
+                        +" </li>\n"
+                        +"<li class=\"nav-item\">\n"
+                        +" <a class=\"nav-link text-white\" href=\"juroscomposto.html\">Juros Composto</a>\n"
+                        +"</li>\n"
+                        +"<li class=\"nav-item\">\n"
+                        +" <a class=\"nav-link disabled\" href=\"#\">Disabled</a>\n"
+                        +"</li>\n"
+                        +"</ul>\n"+
+                        "  </div>\n" +/* fim dos botoes*/
+                        "  <nav class=\"navbar navbar-dark bg-dark\">\n" +
+                        "    <button class=\"navbar-toggler\" type=\"button\" data-toggle=\"collapse\" data-target=\"#navbarToggleExternalContent\" aria-controls=\"navbarToggleExternalContent\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">\n" +
+                        "      <span class=\"navbar-toggler-icon\"></span>\n" +
+                        "    </button>\n" +"  </nav>\n" +
+                        "</div>");
+    /*--- Fim NAVBAR ---*/
+    
+/* INICIO DO CONTEUDO */
+
             out.println("<h1>Servlet JurosSimples at " + request.getContextPath() + "</h1>");
             
             /* Link da pagina Home */
             out.println("<h2><a href=home.html>HOME</a></h2>");            
             /* Fim do link da pagina Home */
             
-            out.println("</body>");
+/* FIM DO CONTEUDO */
+                        
+    /*---- Footer-----------*/
+            out.println("<footer class=\"text-muted bg-dark fim\">\n" +
+                        "<div class=\"container\">\n" +
+                        "<p class=\"float-right \">\n" +
+                        "<a class=\"text-white href=\"#\">Back to top</a>\n" +
+                        "</p>\n" +
+                        "<p>Album example is &copy; Bootstrap, but please download and customize it for yourself!</p>\n" +
+                        "<p>New to Bootstrap? <a class=\"text-white href=\"../../\">Visit the homepage</a> or read our <a class=\"text-white href=\"../../getting-started/\">getting started guide</a>.</p>\n" +
+                        "</div>\n" +
+                        "</footer>");
+    /* --- Fim do Footer------*/
+    
+    /* ----- JS ------*/
+            out.println("<script src=\"https://code.jquery.com/jquery-3.2.1.slim.min.js\" integrity=\"sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN\" crossorigin=\"anonymous\"></script>\n" +
+                "<script src=\"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js\" integrity=\"sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q\" crossorigin=\"anonymous\"></script>\n" +
+                "<script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js\" integrity=\"sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl\" crossorigin=\"anonymous\"></script>");
+    /* -----Fim do JS ----*/
+            
             out.println("</html>");
         }
     }
